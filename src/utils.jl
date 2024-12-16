3accd031984251e846ec95a0497a35b85db44897
pau-torrente <ptbadia@gmail.com>
2024-12-16 22:56:34 +0100

    Tested utils + updated matrix/array structures

--- src/utils.jl
+++ src/utils.jl
@@ -1,65 +1,86 @@
 mutable struct InterpolatingCrosses
-    calI::Vector{Matrix{Float64}}
-    calJ::Vector{Matrix{Float64}}
+    calI::Vector{AbstractMatrix}
+    calJ::Vector{AbstractMatrix}
     cross_sizes::Vector{Int}
     n_var::Int
 
     # TODO Check if calI and calJ should have another nested dimension
     function InterpolatingCrosses(n_var::Int)
-        calI = Vector{Matrix{Float64}}(undef, n_var)
-        calJ = Vector{Matrix{Float64}}(undef, n_var)
-        cross_sizes = Vector{Int}(undef, n_var)
-        calI[1] = Matrix{Float64}(undef, 0, 0)
-        calJ[end] = Matrix{Float64}(undef, 0, 0)
+        calI = Vector{AbstractMatrix}(undef, n_var-1)
+        calJ = Vector{AbstractMatrix}(undef, n_var-1)
+        cross_sizes = [0 for _ in 1:n_var-1]
 
         for k in 1:n_var-1
-            calI[k+1] = Matrix{Float64}(undef, 0, 0)
-            calJ[k] = Matrix{Float64}(undef, 0, 0)
+            calI[k] = Matrix{AbstractMatrix}(undef, 0, 0)
+            calJ[k] = Matrix{AbstractMatrix}(undef, 0, 0)
         end
+        
         new(calI, calJ, cross_sizes, n_var)
     end
 
-    function InterpolatingCrosses(calI::Vector{Vector{Vector{Float64}}}, calJ::Vector{Vector{Vector{Float64}}})
-        if length(calI) != length(calJ)
+    # TODO I am forcing the input to float64, which is probably desired from a numerical point of view, but not very flexible
+    # I am also very unsure about typing things as <:Type
+    function InterpolatingCrosses(calI::Vector{<:AbstractArray{Float64}}, calJ::Vector{<:AbstractArray{Float64}})
+        if size(calI)[1] != size(calJ)[1]
             throw(ArgumentError("calI and calJ must have the same length"))
         end
-        n_var = length(calI)
-        cross_sizes = Vector{Int}(undef, n)
-        for k in 1:n
-            if size(calI[k])[1] != size(calJ[k])[1]
-                throw(ArgumentError("calI and calJ must have the same length, but got mismatch at index $k"))
-            end
-            cross_sizes[k] = size(calI[k])[1]
+        processing_calI = copy(calI)
+        processing_calJ = copy(calJ)
+
+        n_var = size(processing_calI)[1] + 1
+        cross_sizes = Vector{Int}(undef, n_var-1)
+
+        if typeof(processing_calI[1]) <: Vector
+            processing_calI[1] = reshape(processing_calI[1], (1, size(processing_calI[1])[1]))
         end
-        new(calI, calJ, cross_sizes, n_var)
+
+        if typeof(processing_calJ[end]) <: Vector
+            processing_calJ[end] = reshape(processing_calJ[end], (1, size(processing_calJ[end])[1]))
+        end
+
+        for k in 1:n_var-1
+            if length(size(processing_calI[k])) != 2 && length(size(processing_calI[k])) != 2
+                throw(ArgumentError("{I_1,...,I_N-1} and {J_1,...,J_N-1} should be vectors of arrays."))
+            end
+
+            if size(processing_calI[k])[2] != size(processing_calJ[k])[2]
+                throw(ArgumentError("calI and calJ must have the same length, but got mismatch at index $k"))   
+            end    
+
+            cross_sizes[k] = size(processing_calI[k])[2]
+        end
+        new(processing_calI, processing_calJ, cross_sizes, n_var)
     end
 end
 
 struct Grid
-    intervals::Matrix{Float64}
-    points::Matrix{Float64}
+    intervals::Vector{Vector{Float64}}
+    points::Vector{Vector{Float64}}
     points_per_var::Vector{Int}
     n_var::Int
 
-    function Grid(intervals::Matrix{Float64}, points_per_var::Int)
-        if size(intervals)[1] != length(points_per_var)
-            throw(ArgumentError("intervals and points_per_var must have the same length"))
-        end
+    function Grid(intervals::Vector{Vector{Float64}}, points_per_var::Vector{Int})
         n_var = length(intervals)
-        points = Matrix{Float64}(undef, points_per_var, n_var)
+        points = Vector{Vector{Float64}}(undef, n_var)
         for k in 1:n_var
-            points[:, k] = collect(range(intervals[k][1], stop=intervals[k][2], length=points_per_var))
+            points[k] = collect(range(intervals[k][1], stop=intervals[k][2], length=points_per_var[k]))
         end
         new(intervals, points, points_per_var, n_var)
     end
 
-    function Grid(intervals::Matrix{Float64}, points_per_var::Vector{Int})
-        error("Grid with different number of points per variables is not implemented yet.")
+    function Grid(intervals::Vector{Vector{Float64}}, points_per_var::Int)
+        n_var = length(intervals)
+        points = Vector{Vector{Float64}}(undef, n_var)
+        points_per_var_list = [points_per_var for _ in 1:n_var]
+        for k in 1:n_var
+            points[k] = collect(range(intervals[k][1], stop=intervals[k][2], length=points_per_var_list[k]))
+        end
+        new(intervals, points, points_per_var_list, n_var)
     end
 
-    function Grid(points::Matrix{Float64})
+    function Grid(points::Vector{Vector{Float64}})
         n_var = length(points)
-        intervals = Matrix{Float64}(undef, n_var)
+        intervals = Vector{Vector{Float64}}(undef, n_var)
         points_per_var = Vector{Int}(undef, n_var)
         for k in 1:n_var
             intervals[k] = [points[k][1], points[k][end]]
--- test/test_utils.jl
+++ test/test_utils.jl
@@ -4,44 +4,68 @@ include("../src/utils.jl")
 # TODO Adapt tests to Matrix structure instead of nested vectors.
 @testset "InterpolatingCrosses" begin
     @testset "constructor with n_var" begin
-        using .Interpolators
         ic = InterpolatingCrosses(3)
-        @test ic.calI == [[[]], [[]], [[]]]
-        @test ic.calJ == [[[]], [[]], [[]]]
-        @test ic.cross_sizes == [0, 0, 0]
+        @test all(x -> isa(x, AbstractMatrix) && size(x) == (0, 0), ic.calI)
+        @test all(x -> isa(x, AbstractMatrix) && size(x) == (0, 0), ic.calJ)
+        @test size(ic.calI)[1] == 2
+        @test size(ic.calJ)[1] == 2
+        @test ic.cross_sizes == [0, 0]
         @test ic.n_var == 3
     end
 
     # TODO Account for actual meaning of I and J (extremal cases, sizes, nestedness, etc.)
     @testset "constructor with calI and calJ" begin
-        using ..Interpolators
-        calI = [[[0.]], [[1.], [2.]], [[1., 1.], [1., 2.], [2., 2.]], [[1., 2., 3.], [1., 2., 4.]]]
-        calJ = [[[1., 2., 3.], [1., 2., 4.]], [[2., 3.], [2., 5.], [2., 4.]], [[3.], [5.]], [[0.]]]
+        calI = [[1. ; 2.], 
+                [1. 1. 2.; 
+                 1. 2. 2.], 
+                [1. 1.;
+                 2. 2.;
+                 3. 4.]
+        ]
+        
+        calJ = [[1.  1.;
+                 2.  2.;
+                 3.  4.], 
+                 [2.  2.  2.;
+                 3.  5.  4.],
+                 [3.; 5.]
+        ]
+
         ic = InterpolatingCrosses(calI, calJ)
-        @test ic.calI == calI
-        @test ic.calJ == calJ
+        @test ic.calI == [reshape([1. ; 2.], (1,2)), transpose([1. 1. ; 1. 2.; 2. 2.]), transpose([1. 2. 3.; 1. 2. 4.])]
+        @test ic.calJ == [transpose([1. 2. 3.; 1. 2. 4.]), transpose([2. 3.; 2. 5.; 2. 4.]), reshape([3.; 5.], (1, 2))]
         @test ic.cross_sizes == [2, 3, 2]
         @test ic.n_var == 4
     end
 end
 
+
+# TODO Decide whether we should differentiate square grids from non square ones. -> Array indexing seems more performant
 @testset "Grid" begin
     @testset "constructor with intervals and points_per_var" begin
-        using .Interpolators
         intervals = [[0., 1.], [1., 2.], [2., 3.]]
-        points_per_var = [2, 3, 4]
+        points_per_var = [2, 2, 2]
         g = Grid(intervals, points_per_var)
         @test g.intervals == intervals
-        @test g.points == [[0., 1.], [1., 1.5, 2.], [2., 2.5, 3., 3.5]]
+        @test g.points == [[0., 1.], [1., 2.], [2., 3.]]
         @test g.points_per_var == points_per_var
         @test g.n_var == 3
     end
 
+    @testset "constructor with intervals and integer points_per_var" begin
+        intervals = [[0., 1.], [1., 2.], [2., 3.]]
+        points_per_var = 2
+        g = Grid(intervals, points_per_var)
+        @test g.intervals == intervals
+        @test g.points == [[0., 1.], [1., 2.], [2., 3.]]
+        @test g.n_var == 3
+        @test g.points_per_var == [points_per_var for _ in 1:3]
+        
+    end
     @testset "constructor with points" begin
-        using ..Interpolators
         points = [[0., 1.], [1., 1.5, 2.], [2., 2.5, 3., 3.5]]
         g = Grid(points)
-        @test g.intervals == [[0., 1.], [1., 2.], [2., 3.]]
+        @test g.intervals == [[0., 1.], [1., 2.], [2., 3.5]]
         @test g.points == points
         @test g.points_per_var == [2, 3, 4]
         @test g.n_var == 3
